81861194a13fa0474cda01101b4ea94825b41b85
sekak <ahmadesekak@gmail.com>
2025-02-16 14:12:49 +0100

    ``` Refactor authentication provider logic; streamline handling and improve code clarity ```

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -13,7 +13,7 @@ export async function GET(request: Request) {
   if (code) {
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+      // const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       // const isLocalEnv = process.env.NODE_ENV === "development";
 
       // if (isLocalEnv) {
