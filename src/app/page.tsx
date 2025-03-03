9a5b59dcacd16085e4766e1ba116523f574d3a4e
sekak <ahmadesekak@gmail.com>
2025-03-03 17:51:05 +0000

    Refactor posts components; replace Posts with ListPosts and remove unused hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use server'
 import Posts from "@/components/posts/posts";
+import ListPosts from "@/components/reusable/posts";
 
 export default async function page() {
   return (
@@ -9,7 +10,7 @@ export default async function page() {
         <p className="mt-5 text-lg text-justify">
           Discover stories, thinking, and expertise from writers on any topic.
         </p>
-        <Posts/>
+        <ListPosts filter="all"/>
       </div>
     </div>
   );
